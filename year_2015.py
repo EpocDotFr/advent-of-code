2a982d77acb6f746cd163b6135e14b9511a02c09
EpocDotFr <contact@epoc.fr>
2022-10-09 22:13:13 +0200

    Complete puzzle 2015/2

--- year_2015.py
+++ year_2015.py
@@ -31,11 +31,28 @@ class Day2:
             l, w, h = [int(d) for d in box.strip().split('x')]
 
             computed.append(
-                2 * l * w + 2 * w * h + 2 * h * l
+                2 * l * w + 2 * w * h + 2 * h * l + min([l * w, w * h, h * l])
             )
 
         return sum(computed)
 
     @staticmethod
     def level_2(input):
-        return ''
+        computed = []
+
+        for box in input.splitlines():
+            l, w, h = [int(d) for d in box.strip().split('x')]
+
+            d = [l, w, h]
+
+            lowest_1 = min(d)
+
+            d.remove(lowest_1)
+
+            lowest_2 = min(d)
+
+            computed.append(
+                2 * lowest_1 + 2 * lowest_2 + l * w * h
+            )
+
+        return sum(computed)
